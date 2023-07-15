be0fc3f2b13d785830bb809cc823039d3549ad90
Georg <langgeorg61@gmail.com>
2023-07-15 17:05:47 +0200

    Refactored deploy scripts

--- src/scripts/deploy-scripts.ts
+++ src/scripts/deploy-scripts.ts
@@ -1,14 +1,22 @@
 import { NS } from "@ns";
 import { deployScript } from "../lib/scripts-helper";
-import { findServers } from "/lib/server-helper";
+import { scanForServers } from "/lib/server-helper";
 
+/**
+ * Deploys the specified script on the servers it can find, based on the arguments provided.
+ * @param ns Netscript library
+ * @argument {string} script - Script to be deployed
+ * @argument {string} target - Server from which other servers should be scanned
+ * @argument {string} filter - Filters servers with specified string in their name away
+ * @argument {boolean} recursive - Will look for servers recursively
+ */
 export async function main(ns: NS) {
   const script: string = String(ns.args[0]);
   const target: string = String(ns.args[1]);
   const filter: string = String(ns.args[2]);
   const recursive: boolean = Boolean(ns.args[3]);
 
-  const servers: Set<string> = await findServers(ns, target, filter, recursive);
+  const servers: Set<string> = await scanForServers(ns, target, filter, recursive);
   deployScripts(ns, script, servers);
 }
 
@@ -20,7 +28,7 @@ export async function main(ns: NS) {
 export async function deployScripts(ns: NS, script: string, servers: Set<string> | string[]) {
   // Deploy script on all server found
   for (let server of servers) {
-    initServer(ns, server);
+    await initServer(ns, server);
     await deployScript(ns, ns.getServer(server), script);
   }
 }
