5a501f4a85fb5820df829746197ad5d39c122326
Bonesaw <langgeorg61@gmail.com>
2023-08-14 18:36:08 +0200

    Implemented script that starts hacks on other servers

---
+++ src/hack-nodes.ts
@@ -0,0 +1,38 @@
+import { NS, Server} from "@ns";
+
+/** @param {NS} ns */
+export async function main(ns: NS, src: string) {
+	let nodes: string[] = await ns.scan(src);
+    await ns.print(`Found nodes: ${nodes}`);
+
+    for (let node of nodes) {
+        const server: Server = ns.getServer(node)
+        const target = server.hostname;
+
+        const hackLevelRequired = ns.getServerRequiredHackingLevel(target);
+        const hackLevel = ns.getHackingLevel();
+
+        if(hackLevel > hackLevelRequired) {
+            const scriptRam = ns.getScriptRam("hack.js");
+            const availableRam = server.maxRam - server.ramUsed;
+
+            // a threadAmount < 1 causes a runtime error on ns.exec
+            const threadAmount = Math.max(Math.floor(availableRam / scriptRam), 1);
+
+            await init(ns, target);
+
+            await ns.print(`Hacking server ${target} ${threadAmount} times...`);
+            ns.exec("hack.js", target, threadAmount, target);
+        }
+    }
+}
+
+/** @param {NS} ns */
+export async function init(ns:NS, target: string) {
+    await ns.scp("hack.js", target);
+
+    ns.nuke(target);
+    if (ns.fileExists("BruteSSH.exe", target)) {
+        ns.brutessh(target);
+    }
+}
---
+++ src/hack.ts
@@ -0,0 +1,19 @@
+import { NS, Server} from "@ns";
+
+/** @param {NS} ns */
+export async function main(ns: NS) {
+    const target: string = ns.args[0].toString();
+
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+    while(true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
+}
