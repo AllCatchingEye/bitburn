e9d9ec2ed530fba51715e0b05f75cfa609646953
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-08-19 16:14:58 +0200

    deploy-script will now regularly called

--- src/startup.ts
+++ src/startup.ts
@@ -1,8 +1,15 @@
 import { NS } from "@ns";
+import { findServers } from "./lib/find-servers";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-  ns.exec("deploy-script.js", "home");
-  ns.exec("expand-servers.js", "home");
-  ns.exec("hacknet-nodes-upgrader.js", "home");
+  const expandServersPid = ns.exec("expand-servers.js", "home");
+  const hacknetNodesUpgraderPid = ns.exec("hacknet-nodes-upgrader.js", "home");
+
+  const target = "joesguns";
+  while (true) {
+    ns.exec("deploy-script.js", "home", 1, "home", target);
+    await ns.sleep(60000);
+  }
 }
+
