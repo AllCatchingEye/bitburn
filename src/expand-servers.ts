4334b3d6be8063b00b47051e3c1eb67e5909194a
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-08-19 13:58:01 +0200

    Expanded purchase-server to upgrade servers ram after buying them

---
+++ src/expand-servers.ts
@@ -0,0 +1,53 @@
+import { NS } from "@ns";
+
+const SERVER_BASE_NAME: string = "pserv-"
+
+/** @param {NS} ns */
+export async function main(ns: NS) {  
+    const ram = 2;
+    await purchaseAllServers(ns, ram);
+    await upgradeAllServers(ns, ram);
+}
+
+async function purchaseAllServers(ns: NS, ram: number) {
+    for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
+        const purchasedServerName = SERVER_BASE_NAME + i;
+        if (canPurchaseServer(ns, ram)) {
+            ns.purchaseServer(purchasedServerName, ram);
+        }
+
+        await ns.sleep(1000);
+    }
+}
+
+async function upgradeAllServers(ns: NS, ram: number) {
+    for (let exponent = 2; exponent < 20; exponent++) {
+        const ramUpgradeSize = Math.pow(ram, exponent);
+        await upgradeServer(ns, ramUpgradeSize);
+
+        await ns.sleep(1000);
+    }
+}
+
+async function upgradeServer(ns: NS, ramUpgradeSize: number) {
+    for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
+        const serverName = SERVER_BASE_NAME + i;
+        if (canUpgradeServer(ns, serverName, ramUpgradeSize)) {
+            ns.upgradePurchasedServer(serverName, ramUpgradeSize);
+        }
+
+        await ns.sleep(1000);
+    }
+}
+
+function canPurchaseServer(ns: NS, ram: number) {
+    const availableMoney = ns.getServerMoneyAvailable("home");
+    const serverPrice = ns.getPurchasedServerCost(ram);
+    return availableMoney > serverPrice;
+}
+
+function canUpgradeServer(ns: NS, serverName: string, ram: number) {
+    const availableMoney = ns.getServerMoneyAvailable("home");
+    const upgradePrice = ns.getPurchasedServerUpgradeCost(serverName, ram);
+    return availableMoney > upgradePrice;
+}
