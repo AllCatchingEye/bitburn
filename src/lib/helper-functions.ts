25eefb61a2f27ef68956a6090c29b24d2d8dc95d
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-08-22 23:32:23 +0200

    moved functions to helper library

--- src/lib/helper-functions.ts
+++ src/lib/helper-functions.ts
@@ -1,16 +1,34 @@
 import { NS } from "../../NetscriptDefinitions";
 
-export function getMaxPossibleThreads(ns: NS, scriptName: string, host: string) {
-    const scriptRamCost = ns.getScriptRam(scriptName);
-    const availableRamOnServer = (ns.getServerMaxRam(host) - ns.getServerUsedRam(host))
+export function getMaxPossibleThreads(
+  ns: NS,
+  scriptName: string,
+  host: string
+): number {
+  const scriptRamCost = ns.getScriptRam(scriptName);
+  const availableRamOnServer =
+    ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
 
-    const maxPossibleThreads = Math.floor(availableRamOnServer / scriptRamCost);
-
-    return maxPossibleThreads;
+  const maxPossibleThreads = Math.floor(availableRamOnServer / scriptRamCost);
+  return maxPossibleThreads;
 }
 
-export async function waitUntilScriptFinished(ns: NS, pid: number, sleepIntervall: number = 1000) {
-    while (ns.isRunning(pid)) {
-        await ns.sleep(sleepIntervall);
-    }
+export async function waitUntilScriptFinished(
+  ns: NS,
+  pid: number,
+  sleepIntervall = 1000
+): Promise<void> {
+  while (ns.isRunning(pid)) {
+    await ns.sleep(sleepIntervall);
+  }
+}
+
+export function hackLevelEnough(ns: NS, server: string): boolean {
+  const hackLevelRequired = ns.getServerRequiredHackingLevel(server);
+  const hackLevel = ns.getHackingLevel();
+  return hackLevel >= hackLevelRequired;
+}
+
+export function hasSufficientThreads(threadAmount: number): boolean {
+  return threadAmount > 0;
 }
