3721932f0969b1916e352e5b498337dadcf2ceeb
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-08-22 17:04:11 +0200

    Add skeleton Controller

---
+++ src/hacking/hackingController.ts
@@ -0,0 +1,9 @@
+import { NS, Server } from "@ns";
+import { findServers } from "/lib/find-servers";
+
+/** @param {NS} ns */
+export async function main(ns: NS) {
+  ns.disableLog("sleep");
+  
+  
+}
