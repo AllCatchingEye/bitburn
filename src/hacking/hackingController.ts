7933719f4d2143645c6107e33750e5d3f98b31c5
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-08-22 23:33:49 +0200

    Removed old files

--- src/hacking/hackingController.ts
+++
@@ -1,9 +0,0 @@
-import { NS, Server } from "@ns";
-import { findServers } from "/lib/find-servers";
-
-/** @param {NS} ns */
-export async function main(ns: NS) {
-  ns.disableLog("sleep");
-  
-  
-}
