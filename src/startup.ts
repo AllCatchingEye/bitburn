891b5e8408f2fa9185aaee51a2d4495c244b3614
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-09-21 21:28:21 +0200

    Updated startup script

--- src/startup.ts
+++ src/startup.ts
@@ -1,4 +1,5 @@
 import { NS } from "@ns";
+import { getTotalRam } from "./lib/ram-helper";
 
 /**
  * Startup routine for scripts
@@ -6,24 +7,55 @@ import { NS } from "@ns";
  */
 export async function main(ns: NS) {
   const host = "home";
-  if (!ns.scriptRunning("watcher.js", host)) {
-    ns.run("watcher.js");
+
+  startServerExpander(ns);
+
+  if (!ns.scriptRunning("cracker/server-cracker.js", host)) {
+    ns.run("/cracker/server-cracker.js");
   }
 
-  if (!ns.scriptRunning("expand-servers.js", host)) {
-    ns.run("expand-servers.js");
-  }
-
-  if (!ns.scriptRunning("server-cracker.js", host)) {
-    ns.run("server-cracker.js");
-  }
-
-  if (!ns.scriptRunning("/hacking/controller.js", host)) {
-    ns.run("/hacking/controller.js");
-  }
+  startHacking(ns);
 
   if (!ns.scriptRunning("hacknet-nodes-upgrader.js", host)) {
     //ns.run("hacknet-nodes-upgrader.js");
   }
+
+  if (!ns.scriptRunning("watcher.js", host)) {
+    ns.run("watcher.js");
+  }
 }
 
+function startServerExpander(ns: NS) {
+  if (hasEnoughMoney(ns)) {
+    const serverExpander = "expand-servers.js";
+    ns.run(serverExpander);
+  }
+}
+
+function startHacking(ns: NS) {
+  const script = decideHackerScript(ns);
+  if (!ns.isRunning(script)) {
+    ns.run(script);
+  }
+}
+
+function decideHackerScript(ns: NS) {
+  const totalRam = getTotalRam(ns);
+  let script = "";
+  const controller = "/hacking/controller.js";
+  const earlyHack = "/hacking/early-game-hack.js";
+  if (totalRam > 512) {
+    script = controller;
+    ns.scriptKill(earlyHack, "home");
+  } else {
+    script = earlyHack;
+    ns.scriptKill(controller, "home");
+  }
+  return script;
+}
+
+function hasEnoughMoney(ns: NS) {
+  const minMoney = 440_000;
+  const money = ns.getServerMoneyAvailable("home");
+  return money > minMoney;
+}
