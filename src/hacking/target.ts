1fc0c24af6671ae1d64113ec1ce1dd0162739f7e
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-09-24 17:01:27 +0200

    Added documentation to target

--- src/hacking/target.ts
+++ src/hacking/target.ts
@@ -48,7 +48,10 @@ export class Target implements Target {
     this.minDifficulty = server.minDifficulty ?? 0;
   }
 
-  // Updates security and money based on task provided
+  /**
+   * Updates the target for either a whole batch or a single task
+   * @param job - If a batch or single task was executed
+   */
   update(job: Batch | Task): void {
     if (isBatch(job)) {
       job.tasks.forEach((task) => this.determineUpdateType(task));
@@ -57,6 +60,11 @@ export class Target implements Target {
     }
   }
 
+  /**
+   * Determines which type of update should be executed and executes it,
+   * based on the script used in task
+   * @param task - Task which was executed
+   */
   determineUpdateType(task: Task): void {
     switch (task.script) {
       case hackingScripts.Hacking:
@@ -74,6 +82,11 @@ export class Target implements Target {
     }
   }
 
+  /**
+   * Updates money and security on the target,
+   * after a hack script has been executed
+   * @param threads - Amount of threads used by hack script
+   */
   hackUpdate(threads: number): void {
     // update money
     const hackEffect = this.ns.hackAnalyze(this.server.hostname);
@@ -92,6 +105,11 @@ export class Target implements Target {
     this.hackDifficulty = Math.min(this.hackDifficulty + hackSecIncrease, 100); // Max amount of security is 100
   }
 
+  /**
+   * Updates security on the target,
+   * after a weaken script has been executed
+   * @param threads - Amount of threads used by weaken script
+   */
   weakenUpdate(threads: number): void {
     const weakenSecDecrease = this.ns.weakenAnalyze(threads);
 
@@ -102,6 +120,11 @@ export class Target implements Target {
     );
   }
 
+  /**
+   * Updates money and security on the target,
+   * after a grow script has been executed
+   * @param threads - Amount of threads used by grow script
+   */
   growUpdate(threads: number): void {
     // update money
     const growPercent = this.calculateGrowPercent(threads);
@@ -118,9 +141,12 @@ export class Target implements Target {
     this.hackDifficulty = Math.min(this.hackDifficulty + growSecIncrease, 100); // Max amount of security is 100
   }
 
-  // Calculates the growPercent of a given grow Task
-  // Chooses between Formulas based on if Formulas.exe is unlocked
-  // Returns the percent in decimal numbers
+  /**
+   * Calculates the growPercent of a given grow Task
+   * Chooses between Formulas based on if Formulas.exe is unlocked
+   * @param threads - The amount of threads used for grow
+   * @returns Returns the percent in decimal numbers
+   */
   calculateGrowPercent(threads: number): number {
     let growPercent = 0;
     if (this.ns.fileExists("Formulas.exe", "home")) {
@@ -145,7 +171,11 @@ export class Target implements Target {
     return growPercent;
   }
 
-  // Calculates the growPercent by using the by formulas provided function for it
+  /**
+   * Calculates the grow percent by using the by formulas provided function for it
+   * @param threads - The amount of threads used for grow
+   * @returns Grow percent in decimal by provided thread amount
+   */
   simpleFormula(threads: number): number {
     const player = this.ns.getPlayer();
     const server = this.ns.getServer(this.server.hostname);
@@ -158,7 +188,12 @@ export class Target implements Target {
     return growPercent;
   }
 
-  // Checks if a server is prepared for a batch
+  /**
+   * Checks if the target is prepared for batching.
+   * Its prepared if its money is at the maximum
+   * and security at the minimum
+   * @returns If the server is prepared for batching
+   */
   isPrepped(): boolean {
     const moneyPrepped = this.moneyAvailableIsPrepped();
     const secPrepped = this.hackDifficultyIsPrepped();
@@ -167,11 +202,19 @@ export class Target implements Target {
     return isPrepped;
   }
 
+  /**
+   * Checks if the money of the target lies at maximum for batching
+   * @returns If the money is prepared
+   */
   moneyAvailableIsPrepped(): boolean {
     const moneyIsPrepped = this.moneyAvailable === this.moneyMax;
     return moneyIsPrepped;
   }
 
+  /**
+   * Checks if the security of the target is a the minimum for batching
+   * @returns If the security is prepared
+   */
   hackDifficultyIsPrepped(): boolean {
     const secToFix = Math.abs(this.hackDifficulty - this.minDifficulty);
 
